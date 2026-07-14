ac6a1f2cab7d38bcf237a9ae6609c332eaab1014
Niloy Sarkar <niloy.sarkar@g.bracu.ac.bd>
2026-07-14 16:21:44 +0600

    Add log upload functionality

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if ROOT_DIR not in sys.path:
@@ -24,13 +24,12 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///securewatch.db"
 
 db.init_app(app)
 
+
 @app.route("/")
 def dashboard():
 
     total_events = Event.query.count()
-
     total_alerts = Alert.query.count()
-
     alerts = Alert.query.all()
 
     return render_template(
@@ -40,9 +39,41 @@ def dashboard():
         alerts=alerts
     )
 
+
+# NEW ROUTE
+@app.route("/upload", methods=["GET", "POST"])
+def upload_log():
+
+    if request.method == "POST":
+
+        log_file = request.files.get("file")
+
+        if log_file:
+
+            os.makedirs("sample_logs", exist_ok=True)
+
+            filepath = os.path.join(
+                "sample_logs",
+                log_file.filename
+            )
+
+            log_file.save(filepath)
+
+            return redirect("/")
+
+    return """
+    <h2>SecureWatch Log Upload</h2>
+
+    <form method="POST" enctype="multipart/form-data">
+        <input type="file" name="file">
+        <button type="submit">Upload</button>
+    </form>
+    """
+
+
 if __name__ == "__main__":
 
     with app.app_context():
         db.create_all()
 
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
